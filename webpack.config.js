32dfd3608a40719c480ca38cc98cc8b8d2b86612
dimacia666 <dimahotab666@gmail.com>
2026-02-07 10:53:10 +0200

    Changed configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -40,6 +41,11 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "styles.css",
     }),
+    new CopyWebpackPlugin({
+    patterns: [
+      { from: "src/assets/svg", to: "assets/svg" } // 
+    ]
+  })
   ],
 };
 
