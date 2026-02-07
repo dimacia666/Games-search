e83ab1ca4e6b21b59c14fea5ee620702716b14a0
dimacia666 <dimahotab666@gmail.com>
2026-02-07 10:55:25 +0200

    Rename baner

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 //Импорт стилей
 import './styles/main.scss';
 
-import './assets/svg/icon.svg'
 
 //Импорт банера
-import bannerImg from './assets/images/af49acb6dcc48f0ee7a6b6a193220ae7e61e1076.png';
+import bannerImg from './assets/images/baner.png';
 const banner = document.querySelector('.baner');
 banner.src = bannerImg;
